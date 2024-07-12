db592c47255290791b42aee1a73baca02b38d3ae
UsmanMern <mu1363131@gmail.com>
2024-07-12 23:58:04 +0500

    learning

---
+++ dictionaries.py
@@ -0,0 +1,38 @@
+students = {"name": "Saim", "roll_no": "2521306"}
+# students["name"] = "Usman"
+# print(students["name"])
+
+# for student in students:
+#     print(student,students[student])
+
+
+# for key, value in students.items():
+#     print(key, value)
+
+# if "name" in students:
+#     print("record have student name")
+
+# print(len(students))
+
+# students["semester"] = "7th Eve"
+
+# students.pop("name")
+# students.popitem()
+
+# del students["name"]
+
+# students_copy = students.copy()
+# print(students_copy)
+
+# new_info = {
+#     "skills": "ML and data sceince",
+#     "proffections": "software Engineer and Developer",
+# }
+# students.update(new_info)
+
+# students.clear()
+students_copy = students.copy()
+students_copy["name"] = "usman"
+print(students_copy)
+print(students)
+# students_record = {"name", {}, {}, {}}
---
+++ list.py
@@ -0,0 +1,35 @@
+students = ["usman", "saim", "ali", "mani"]
+
+# print(students)
+# print(students[-1])
+# print(students[1:2])
+# print(students[:2])
+
+# students[3] = "Fahad"
+
+# students[1:2] = ["Fahad"]
+# students[1:3] = ["Fahad", "Arghaan"]
+# students[1:1] = ["Fahad", "Arghaan"]
+# students[1:3] = []
+
+# for student in students:
+#     print(student, end=" ")
+
+
+# if "usman" in students:
+# print("usman is present ")
+
+# students.append("Fahad")
+
+# students.pop()
+
+# students.remove("saim")
+
+# students_copy = students.copy()
+# students.insert(1, "Ali")
+# print(students_copy)
+# print(students)
+
+squared_nums = [x**2 for x in range(10)]
+
+print(squared_nums)
