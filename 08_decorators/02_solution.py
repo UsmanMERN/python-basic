4fcca63c746f34a94a06137efa296730f234f64c
UsmanMERN <mu1363131@gmail.com>
2024-07-21 11:47:23 +0500

    i think basic done

--- 08_decorators/02_solution.py
+++ 08_decorators/02_solution.py
@@ -1,17 +1,25 @@
 def debug(func):
     def wrapper(*args, **kwargs):
-        return func(*args)
+        args_value = ", ".join(str(arg) for arg in args)
+        kwargs_value = ", ".join(f"{k}={v}" for k, v in kwargs.items())
+        print(
+            f"calling: {func.__name__} with args {args_value} and kwargs {kwargs_value}"
+        )
+        return func(*args, **kwargs)
 
     return wrapper
 
 
+@debug
 def hello():
     print("Hello")
 
 
+@debug
 def greet(name, greeting="Hello"):
     print(f"{greeting} {name}")
 
 
+hello()
 greet("Muhammad Usman", "Good Evening")
 # print(greet("Muhammad Usman", "Asslam o Alaikum"))
